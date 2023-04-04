d977e3a9ce704bd9a35fb1613f87924fa2ad058c
Maniyavel <116820894+Maniyavel@users.noreply.github.com>
2023-04-04 20:07:36 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// доступ до елемента
+const gallery=document.querySelector(".gallery");
+//Створюю розмітку
+function createGalleryItem(galleryItems){
+    return galleryItems.map(({preview, original,description})=>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+       
+        alt="${description}"
+      />
+    </a>
+  </li>`)
+  .join('');
+}
 
-console.log(galleryItems);
+//Додаю розмітку в дерево
+const items=createGalleryItem(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", items);
+const lightbox = new SimpleLightbox('.gallery a', 
+{ 
+    captionsData: "alt",
+    captionDelay: 250,
+
+ });
+
+ console.log(galleryItems);
